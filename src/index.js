c68ddce5a30022ffbee5804b667d55b9f0466277
Gautham Rajesh <gauthamr@utexas.edu>
2022-04-14 19:44:53 -0500

    webflow exported

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import IndexView from './views/IndexView';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <IndexView />
   </React.StrictMode>,
   document.getElementById('root')
 );
